9ba248bcd720df34a61022b930a93b6155c804cf
MburuEric <99777790+MburuEric@users.noreply.github.com>
2022-03-15 04:34:40 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,22 +1,74 @@
-/*
-    project: county library management system.
-    Author:  Titus Njiru
-    Date:    Oct, 2021
-    Compiler: GNU GCC
-    Language: C99
-    License: MIT
-*/
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <windows.h>
+int menu();
 int main()
 {
-
-        printf("\tCounty Library\n");
-        printf("Welcome Mr. Eric. \n");
-        printf("what would you like to do?\n");
-        printf("1. Add new User\n");
-        printf("2. Add book. \n");
-        printf("selected Action: ");
-        return 0;
+    int action;
+    action = menu();
+    execute_action(action);
+    return 0;
 }
+
+void execute_action(int action) {
+    printf("you selected action %d",action);
+}
+int menu() // header/ prototype
+{
+     int action;
+    do {
+        printf("\tCounty Library\n");
+        printf("Welcome   Mr. Eric\n");
+        printf("what would you like to do?\n");
+        printf("1. view user\n");
+        printf("2. Add new User\n");
+        printf("3. Edit User\n");
+        printf("4. Delete user\n");
+        printf("5. Change Password\n");
+        printf("6. Log out\n");
+        printf("7. Exit System\n");
+        printf("selected Action:");
+        scanf("%d",&action);
+        if (action < 1 || action > 7) {
+            system("cls");
+            printf ("invalid action ! ! ! Try again.\n");
+        }
+    } while (action < 1 || action > 7);
+
+void execute_action(int action);switch(action) {
+     case 1:
+       printf("list of users\n");
+     break;
+     case 2:
+       printf("adding a new user\n");
+       break;
+     case 3:
+       printf("Editing user\n");
+       break;
+     case 4:
+       printf("deleting user\n");
+       break;
+     case 5:
+       printf("changing password\n");
+       break;
+     case 6:
+       printf("you have been logged out\n");
+       break;
+     case 7:
+       printf("bye\n");
+       break;
+     default:
+       printf("invalid action\n");
+     }
+
+     void close() {
+         printf("thanks for using the system\n");
+         printf("created by Eric Mburu\n");
+         printf("bye\n");
+         Sleep(2000);
+         exit(0);
+     }
+
+    }
+
+
